0ad6ddfdcb0f1e70b6f88efc492d4a0b8da5e3d8
Juan Pablo Betancur M <jpbetancur1999@gmail.com>
2021-09-29 11:21:33 -0500

    Hice varios cambios en el diseño, entre ellos: 1. Cambiar el fondo; 2. Cambiar la estructura de los botones de las figuras; 3. Crear funciones para hacer el manejo de cambio de color más dinámico y fácil; 4. Intentar crear cuadro interno de la primera figura

--- canvas/challenge/challenge.js
+++ canvas/challenge/challenge.js
@@ -1,13 +1,7 @@
 const nmr_lines = document.getElementById("nmr_lines");
-
-const btn_create = document.getElementById("sv_data");
-btn_create.addEventListener("click", createByClick);
-
-const btn_clear = document.getElementById("rm_data");
-btn_clear.addEventListener("click", clearCanvas);
-
-// const btn_color = document.getElementsByClassName("btn_color");
-// btn_color.addEventListener("click", colorCanvas);
+let color_figures = "#ffffff";
+// const btn_clear = document.getElementById("rm_data");
+// btn_clear.addEventListener("click", clearCanvas);
 
 const c = document.getElementById("canvas_figure");
 const canvas = c.getContext("2d");
@@ -23,11 +17,16 @@ function drawCanvasLines(color, xinicial, yinicial, xfinal, yfinal)
     canvas.closePath();
 }
 
-// drawCanvasLines("#ff0000", 150,150,0,0);
-
-function createByClick()
+function squareByClick(color)
 {
     clearCanvas();
+
+    if(color == "All" || color == undefined)
+    {
+        color_figures = colorRandom();
+    }else{
+        color_figures = color;
+    }
 
     let lines = parseInt(nmr_lines.value);
     let l = 0;
@@ -42,25 +41,48 @@ function createByClick()
         xf = width_canvas - (space * l );
         yf = space * (l + 1);
 
-        drawCanvasLines("#ffffff", xi, 0, width_canvas, yf);
-        drawCanvasLines("#fff000", width_canvas, yi, xf, width_canvas);
-        drawCanvasLines("#000000", 1, yi, (space * l), size_width);
-        drawCanvasLines("#ff0000", xi, 1, 1, width_canvas - (space * l ));
+        drawCanvasLines(color_figures, xi, 0, width_canvas, yf);
+        drawCanvasLines(color_figures, width_canvas, yi, xf, width_canvas);
+        drawCanvasLines(color_figures, 1, yi, (space * l), size_width);
+        drawCanvasLines(color_figures, xi, 1, 1, width_canvas - (space * l ));
 
+        color == "All" ? color_figures = colorRandom() : color_figures;
     }
-    drawCanvasLines("#ffffff", 1, 1, size_width, 1);
-    drawCanvasLines("#ffffff", size_width, 1, size_width, size_width);
-    drawCanvasLines("#fff000", size_width, 1, size_width, size_width);
-    drawCanvasLines("#fff000", size_width, size_width, 1, size_width);
-    drawCanvasLines("#000000", size_width, size_width, 1, size_width);
-    drawCanvasLines("#000000", 1, size_width, 1, 1);
-    drawCanvasLines("#ff0000", 1, 1, size_width, 1);
-    drawCanvasLines("#ff0000", 1, size_width, 1, size_width);
 
-    drawCanvasLines("#000000", 80, 80, 220, 80);
-    drawCanvasLines("#000000", 80, 80, 80, 220);
-    drawCanvasLines("#000000", 80, 220, 220, 220);
-    drawCanvasLines("#000000", 220, 220, 220, 80);
+    drawCanvasLines(color_figures, 1, 1, size_width, 1);
+    drawCanvasLines(color_figures, size_width, 1, size_width, size_width);
+    drawCanvasLines(color_figures, size_width, 1, size_width, size_width);
+    drawCanvasLines(color_figures, size_width, size_width, 1, size_width);
+    drawCanvasLines(color_figures, size_width, size_width, 1, size_width);
+    drawCanvasLines(color_figures, 1, size_width, 1, 1);
+    drawCanvasLines(color_figures, 1, 1, size_width, 1);
+    drawCanvasLines(color_figures, 1, size_width, 1, size_width);
+
+    // Cuadro interno
+    let width_little = parseInt(140);
+    let xli,yli,xlf,ylf;
+    let space_little = width_little / lines;
+
+    for(let $i = 0; $i < lines; $i++)
+    {
+        xli = space_little * $i;
+        ylf = width_little - (space_little * $i );
+
+        drawCanvasLines(color_figures, xli, 80, 80, ylf);
+
+        color == "All" ? color_figures = colorRandom() : color_figures;
+    }
+
+    drawCanvasLines(color_figures, 80, 80, 220, 80);
+    drawCanvasLines(color_figures, 80, 80, 80, 220);
+    drawCanvasLines(color_figures, 80, 220, 220, 220);
+    drawCanvasLines(color_figures, 220, 220, 220, 80);
+
+
+    // drawCanvasLines(color_figures, 90, 80, 80, 210);
+    // drawCanvasLines(color_figures, 100, 80, 80, 200);
+    // drawCanvasLines(color_figures, 110, 80, 80, 190);
+    // drawCanvasLines(color_figures, 120, 80, 80, 180);
 
 }
 
@@ -69,7 +91,91 @@ function clearCanvas()
     canvas.clearRect(0,0,c.width,c.height);
 }
 
-// function colorCanvas()
+function createFigure(type)
+{
+    // const btn_create = document.getElementById("sv_data");
+    // btn_create.addEventListener("click", squareByClick);
+
+    clearCanvas();
+
+    switch (type) {
+        case "id_buterfly":
+            
+            break;
+        case "id_eye":
+        
+            break;
+        case "id_name":
+    
+            break;
+        case "id_square":
+            squareByClick();
+            // document.addEventListener('keyup', squareByClick);
+
+            break;
+        case "id_star":
+    
+            break;
+        case "id_vortice":
+    
+            break;
+        default:
+            break;
+    }
+}
+
+function colorCanvas(value)
+{
+    switch (value) {
+        case "White":
+            colorAllFigures("#ffffff");
+            break;
+        case "Orange":
+            colorAllFigures("#fc5f04");
+            break;
+        case "Red":
+            colorAllFigures("#ff0000");
+            break;
+        case "Yellow":
+            colorAllFigures("#fff000");
+            break;
+        case "All":
+            colorAllFigures("All");
+            break;
+        default:
+            break;
+    }
+}
+
+function colorAllFigures(color_param)
+{
+    squareByClick(color_param);
+
+}
+
+// function colorRandom()
 // {
-//     console.log('Un mensaje ' + btn_color.value);
+//     let colors = new Array();
+//     colors = ['#ffffff', '#000000','#ff0000','#fff000','#fc5f04'];
+//     let position = (Math.random() * 5).toFixed(0);
+
+//     return colors[position];
+
 // }
+
+function generateLetters()
+{
+    let letters = ["a","b","c","d","e","f","0","1","2","3","4","5","6","7","8","9"];
+    let position = (Math.random() * 15).toFixed(0);
+    return letters[position];
+}
+
+function colorRandom()
+{
+    let colorHex = "";
+    for(let $i= 0; $i < 6; $i++)
+    {
+        colorHex = colorHex + generateLetters();
+    }
+    return "#" + colorHex;
+}
