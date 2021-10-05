39819e81049227cccd1cb7d4483b77facc8c9adc
Juan Pablo Betancur M <jpbetancur1999@gmail.com>
2021-10-05 17:28:58 -0500

    Se comentó todo el código siguiendo las buenas prácticas y además se hicieron algunos retoques de diseño

--- canvas/challenge/challenge.js
+++ canvas/challenge/challenge.js
@@ -1,10 +1,23 @@
+/*
+ * Global variables
+ */
 const nmr_lines = document.getElementById("nmr_lines");
 let color_figures = "#ffffff";
+let coord_1, coord_2, coord_3, coord_4, coord_5, coord_6, coord_7;
 
 const c = document.getElementById("canvas_figure");
 const canvas = c.getContext("2d");
 const width_canvas = c.width;
 
+/**
+ * @function drawCanvasLines Función para trazar lineas
+ * @param {color} color Recibe el color de las lineas a trazar 
+ * @param {xinicial} xinicial Recibe el punto X de partida
+ * @param {yinicial} yinicial Recibe el punto Y de partida
+ * @param {xfinal} xfinal Recibe el punto X final
+ * @param {yfinal} yfinal Recibe el punto Y final
+ * @author PabloskyDev
+ */
 function drawCanvasLines(color, xinicial, yinicial, xfinal, yfinal)
 {
     canvas.beginPath();
@@ -15,6 +28,16 @@ function drawCanvasLines(color, xinicial, yinicial, xfinal, yfinal)
     canvas.closePath();
 }
 
+/**
+ * @function drawCanvasCircle Función para trazar circulos
+ * @param colorCircle Recibe el color del circulo
+ * @param centerX Recibe la coordenada X del centro
+ * @param centerY Recibe la coordenada Y del centro
+ * @param radio Recibe el radio del circulo
+ * @param angleI Recibe el angulo inicial del circulo
+ * @param angleF Recibe el angulo final del circulo
+ * @author PabloskyDev
+ */
 function drawCanvasCircle(colorCircle,centerX, centerY, radio, angleI , angleF)
 {
     canvas.beginPath();
@@ -28,6 +51,119 @@ function drawCanvasCircle(colorCircle,centerX, centerY, radio, angleI , angleF)
     canvas.closePath();
 }
 
+/**
+ * @function clearCanvas Limpia el canvas
+ * @author PabloskyDev
+ */
+function clearCanvas()
+{
+    canvas.clearRect(0,0,c.width,c.height);
+}
+
+/**
+ * @function createFigure Asigna según un click que figura ha de abrirse.
+ * @param type Recibe el id de la figura seleccionada
+ * @author PabloskyDev
+ */
+function createFigure(type)
+{
+    clearCanvas();
+
+    switch (type) {
+        case "id_flower":
+            flowerByClick();
+            break;
+        case "id_eye":
+            eyeByClick();
+            break;
+        case "id_square":
+            squareByClick();
+            break;
+        case "id_star":
+            starByClick();
+            break;
+        case "id_vortice":
+            vorticeByClick();
+            break;
+        default:
+            break;
+    }
+}
+
+/**
+ * @function colorCanvas Cambia el color del canvas(lineas)
+ * @param value Recibe el nombre del color que pintará las lineas del canvas
+ * @author PabloskyDev
+ */
+function colorCanvas(value)
+{
+    switch (value) {
+        case "White":
+            colorAllFigures("#ffffff");
+            break;
+        case "Orange":
+            colorAllFigures("#fc5f04");
+            break;
+        case "Red":
+            colorAllFigures("#ff0000");
+            break;
+        case "Yellow":
+            colorAllFigures("#fff000");
+            break;
+        case "All":
+            colorAllFigures("All");
+            break;
+        default:
+            break;
+    }
+}
+
+/**
+ * @function colorAllFigures Cambia el color de las lineas que componen cada figura
+ * @param color_param Recibe el color con que se pintará cada figura
+ * @author PabloskyDev
+ */
+function colorAllFigures(color_param)
+{
+    squareByClick(color_param);
+    starByClick(color_param);
+    vorticeByClick(color_param);
+    eyeByClick(color_param);
+    flowerByClick(color_param);
+}
+
+/**
+ * @function generateLetters Crea array que almacena los valores para crear color hexadecimal
+ * @returns Retorna alguna letra o número del array
+ * @author PabloskyDev
+ */
+function generateLetters()
+{
+    let letters = ["a","b","c","d","e","f","0","1","2","3","4","5","6","7","8","9"];
+    let position = (Math.random() * 15).toFixed(0);
+    return letters[position];
+}
+
+/**
+ * @function colorRandom Asigna según un ciclo colores random
+ * @returns Retorna la composición de un color hexadecimal
+ * @author PabloskyDev
+ */
+function colorRandom()
+{
+    let colorHex = "";
+    for(let $i= 0; $i < 6; $i++)
+    {
+        colorHex = colorHex + generateLetters();
+    }
+    return "#" + colorHex;
+}
+
+/**
+ * @function squareByClick Forma un cuadrado
+ * @param color Recibe el color de las lineas que pintan la figura
+ * @author PabloskyDev
+ */
 function squareByClick(color)
 {
     clearCanvas();
@@ -41,21 +177,19 @@ function squareByClick(color)
 
     let lines = parseInt(nmr_lines.value);
     let l = 0;
-    let xi, yi, xf, yf;
     let space = width_canvas / lines;
     let size_width = width_canvas - 1;
 
     for(l = 0; l < lines; l++)
     {
-        xi = space * l;
-        yi = space * l;
-        xf = width_canvas - (space * l );
-        yf = space * (l + 1);
+        coord_1 = space * l;
+        coord_3 = width_canvas - (space * l );
+        coord_4 = space * (l + 1);
 
-        drawCanvasLines(color_figures, xi, 0, width_canvas, yf);
-        drawCanvasLines(color_figures, width_canvas, yi, xf, width_canvas);
-        drawCanvasLines(color_figures, 1, yi, (space * l), size_width);
-        drawCanvasLines(color_figures, xi, 1, 1, width_canvas - (space * l ));
+        drawCanvasLines(color_figures, coord_1, 0, width_canvas, coord_4);
+        drawCanvasLines(color_figures, width_canvas, coord_1, coord_3, width_canvas);
+        drawCanvasLines(color_figures, 1, coord_1, coord_1, size_width);
+        drawCanvasLines(color_figures, coord_1, 1, 1, coord_3);
 
         color == "All" ? color_figures = colorRandom() : color_figures;
     }
@@ -68,20 +202,20 @@ function squareByClick(color)
 
     // Cuadro interno
     let width_little = parseInt(220);
-    let xli,ylf;
     let space_little = width_little / lines;
 
     for(let $i = 0; $i < lines; $i++)
     {
-        xli = 80 + (space_little * $i);
-        ylf = width_little - (space_little * $i );
+        coord_1 = 80 + (space_little * $i);
+        coord_2 = width_little - (space_little * $i );
+        coord_3 = 80 + (space_little * $i);
 
-        if((xli > 81 && xli < 221) || (ylf > 81 && ylf < 221))
+        if((coord_1 > 81 && coord_1 < 221) || (coord_2 > 81 && coord_2 < 221))
         {
-            drawCanvasLines(color_figures, xli, 80, 80, ylf);
-            drawCanvasLines(color_figures, xli, 80, 220, (80 + (space_little * $i)));
-            drawCanvasLines(color_figures, xli, 220, 220, ylf);
-            drawCanvasLines(color_figures, xli, 220, 80, (80 + (space_little * $i)));
+            drawCanvasLines(color_figures, coord_1, 80, 80, coord_2);
+            drawCanvasLines(color_figures, coord_1, 80, 220, coord_3);
+            drawCanvasLines(color_figures, coord_1, 220, 220, coord_2);
+            drawCanvasLines(color_figures, coord_1, 220, 80, coord_3);
         }
 
         color == "All" ? color_figures = colorRandom() : color_figures;
@@ -93,6 +227,11 @@ function squareByClick(color)
     drawCanvasLines(color_figures, 220, 220, 220, 80);
 }
 
+/**
+ * @function starByClick Forma una estrella
+ * @param colorStar Recibe el color de las lineas que pintan la figura
+ * @author PabloskyDev
+ */
 function starByClick(colorStar)
 {
     clearCanvas();
@@ -106,7 +245,6 @@ function starByClick(colorStar)
 
     let lines = parseInt(nmr_lines.value);
     let l = 0;
-    let coord_1, coord_2, coord_3, coord_4,coord_5;
     let space = width_canvas / lines;
     let width_middle = width_canvas/2;
 
@@ -161,6 +299,11 @@ function starByClick(colorStar)
     drawCanvasLines(color_figures, width_middle, width_middle, 1, width_middle);
 }
 
+/**
+ * @function vorticeByClick Forma un vórtice
+ * @param colorVortice Recibe el color de las lineas que pintan la figura
+ * @author PabloskyDev
+ */
 function vorticeByClick(colorVortice)
 {
     clearCanvas();
@@ -174,7 +317,6 @@ function vorticeByClick(colorVortice)
 
     let lines = parseInt(nmr_lines.value);
     let l = 0;
-    let coord_1, coord_2, coord_3, coord_4,coord_5;
     let space = width_canvas / lines;
     let width_middle = width_canvas/2;
 
@@ -207,6 +349,11 @@ function vorticeByClick(colorVortice)
     drawCanvasLines(color_figures, width_middle, width_middle, 1, width_middle);
 }
 
+/**
+ * @function eyeByClick Forma un ojo
+ * @param colorEye Recibe el color de las lineas que pintan la figura
+ * @author PabloskyDev
+ */
 function eyeByClick(colorEye)
 {
     clearCanvas();
@@ -220,7 +367,6 @@ function eyeByClick(colorEye)
 
     let lines = parseInt(nmr_lines.value);
     let l = 0;
-    let coord_1;
     let space = width_canvas / lines;
     let width_middle = width_canvas/2;
 
@@ -250,6 +396,11 @@ function eyeByClick(colorEye)
     }
 }
 
+/**
+ * @function flowerByClick Forma una flor
+ * @param colorFlower Recibe el color de las lineas que pintan la figura
+ * @author PabloskyDev
+ */
 function flowerByClick(colorFlower)
 {
     clearCanvas();
@@ -263,7 +414,6 @@ function flowerByClick(colorFlower)
 
     let lines = parseInt(nmr_lines.value);
     let l = 0;
-    let coord_1, coord_2, coord_3, coord_4, coord_5, coord_6, coord_7;
     let space = width_canvas / lines;
     let width_middle = width_canvas/2;
     let middle_little = width_middle/2;
@@ -349,95 +499,4 @@ function flowerByClick(colorFlower)
         }
         colorFlower == "All" ? color_figures = colorRandom() : color_figures;
     }
-}
-
-
-function clearCanvas()
-{
-    canvas.clearRect(0,0,c.width,c.height);
-}
-
-function createFigure(type)
-{
-    clearCanvas();
-
-    switch (type) {
-        case "id_flower":
-            flowerByClick();
-            break;
-        case "id_eye":
-            eyeByClick();
-            break;
-        case "id_square":
-            squareByClick();
-            break;
-        case "id_star":
-            starByClick();
-            break;
-        case "id_vortice":
-            vorticeByClick();
-            break;
-        default:
-            break;
-    }
-}
-
-function colorCanvas(value)
-{
-    switch (value) {
-        case "White":
-            colorAllFigures("#ffffff");
-            break;
-        case "Orange":
-            colorAllFigures("#fc5f04");
-            break;
-        case "Red":
-            colorAllFigures("#ff0000");
-            break;
-        case "Yellow":
-            colorAllFigures("#fff000");
-            break;
-        case "All":
-            colorAllFigures("All");
-            break;
-        default:
-            break;
-    }
-}
-
-function colorAllFigures(color_param)
-{
-    console.log(this.value);
-    squareByClick(color_param);
-    starByClick(color_param);
-    vorticeByClick(color_param);
-    eyeByClick(color_param);
-    flowerByClick(color_param);
-}
-
-// function colorRandom()
-// {
-//     let colors = new Array();
-//     colors = ['#ffffff', '#000000','#ff0000','#fff000','#fc5f04'];
-//     let position = (Math.random() * 5).toFixed(0);
-
-//     return colors[position];
-
-// }
-
-function generateLetters()
-{
-    let letters = ["a","b","c","d","e","f","0","1","2","3","4","5","6","7","8","9"];
-    let position = (Math.random() * 15).toFixed(0);
-    return letters[position];
-}
-
-function colorRandom()
-{
-    let colorHex = "";
-    for(let $i= 0; $i < 6; $i++)
-    {
-        colorHex = colorHex + generateLetters();
-    }
-    return "#" + colorHex;
 }
